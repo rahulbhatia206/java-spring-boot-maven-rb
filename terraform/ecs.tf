0d15b4a2877b0c243642e7b324f06ba480cce305
Rahul Bhatiya <rahul.bhatiya@numerator.com>
2023-01-30 02:29:53 +0530

    changing cluster

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "techverito-ecs-cluster" {
-  name = "${var.name}-ecs-cluster-3"
+  name = "${var.name}-ecs-cluster"
 
   setting {
     name  = "containerInsights"
@@ -7,52 +7,16 @@ resource "aws_ecs_cluster" "techverito-ecs-cluster" {
   }
 }
 
-#module "ecs-fargate" {
-#  source  = "umotif-public/ecs-fargate/aws"
-#  version = "~> 6.1.0"
-#
-#  name_prefix        = "techverito-ecs-service-2"
-#  vpc_id             = aws_vpc.ecs_vpc.id
-#  private_subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-#
-#  cluster_id = aws_ecs_cluster.techverito-ecs-cluster.id
-#
-#  task_container_image   = "136052446758.dkr.ecr.us-east-2.amazonaws.com/techverito:latest"
-#  task_definition_cpu    = 256
-#  task_definition_memory = 512
-#
-#  task_container_port             = 80
-#  task_container_assign_public_ip = true
-#
-#  load_balanced = false
-#
-#  target_groups = [
-#    {
-#      target_group_name = "tg-fargate-example"
-#      container_port    = 80
-#    }
-#  ]
-#
-#  health_check = {
-#    port = "traffic-port"
-#    path = "/"
-#  }
-#
-#  tags = var.tags
-#}
-
-
-# update file container-def, so it's pulling image from ecr
-resource "aws_ecs_task_definition" "techverito-task-definition-3" {
-  family                = "techverito-taskdef-3"
+resource "aws_ecs_task_definition" "techverito-task-definition" {
+  family                = "${var.name}-ecs"
   container_definitions = file("container-definitions/container-def.json")
   tags = var.tags
 }
 
 resource "aws_ecs_service" "service" {
-  name            = "web-service"
+  name            = "${var.name}-ecs-service"
   cluster         = aws_ecs_cluster.techverito-ecs-cluster.id
-  task_definition = aws_ecs_task_definition.techverito-task-definition-3.arn
+  task_definition = aws_ecs_task_definition.techverito-task-definition.arn
   desired_count   = 10
 
   # Optional: Allow external changes without Terraform plan difference(for example ASG)
